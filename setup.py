8cc4f05d86d4bd1cd5617bb53e2b214c639907cb
Dark Envoy <Apollon76@yandex.ru>
2019-01-16 21:27:38 +0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dempsters_algorithm",
+    version="0.0.2",
+    description="Dempster's covariance selection algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Apollon76/dempster_algorithm",
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
